5a7fec3d82217a02bf453d67b1170f496dce92f6
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2026-02-07 18:03:11 -0300

    updating recource regions

--- vars/AWSResources.groovy
+++ vars/AWSResources.groovy
@@ -5,14 +5,38 @@ def call (String regiSel, String res_sel) {
             case 'N. Virginia':
                 regiSel='us-east-1'
             break
-            case 'apac':
+            case 'Ohio'
                 regiSel='us-east-1'
             break
-            case 'europe':
-                regiSel='us-east-1'
+            case 'N. Carolina'
+                regiSel='us-west-1'
+            break
+            case 'Oregon'
+                regiSel='us-west-2'
+            break
+            case 'Mumbai':
+                regiSel='ap-south-1'
+            break
+            case 'Singapore':
+                regiSel='ap-southeast-1'
+            break
+            case 'Sydney':
+                regiSel='ap-southeast-1'
+            break
+            case 'Tokyo':
+                regiSel='ap-northeast-1'
+            break
+            case 'Frankfurt':
+                regiSel='eu-central-1'
+            break
+            case 'London':
+                regiSel='eu-west-2'
+            break
+            case 'Stockholm':
+                regiSel='eu-north-1'
             break
             case 'São Paulo':
-                regiSel='us-east-1'
+                regiSel='sa-east-1'
             break
         }
         switch (params.resource) {
