48e3c87e8e3b013299aecf79c172ae2d29ef542e
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2026-03-09 22:02:18 -0300

    improving all script

--- vars/AWSResources.groovy
+++ vars/AWSResources.groovy
@@ -1,31 +1,37 @@
-def call (String regiSel, String res_sel, String res_na) {
-    
-    stage('Input Parameters') {
-        def regions = [
-            'N. Virginia' : 'us-east-1',
-            'Ohio'        : 'us-east-2',
-            'N. Carolina' : 'us-west-1',
-            'Oregon'      : 'us-west-2',
-            'Mumbai'      : 'ap-south-1',
-            'Singapore'   : 'ap-southeast-1',
-            'Sydney'      : 'ap-southeast-1',
-            'Tokyo'       : 'ap-northeast-1',
-            'Frankfurt'   : 'eu-central-1',
-            'London'      : 'eu-west-2',
-            'Stockholm'   : 'eu-north-1',
-            'São Paulo'   : 'sa-east-1'
-        ]
-        def resources = [
-            vpc       : 'vpc',
-            instance  : 'instance',
-            container : 'container',
-            s3        : 's3'
-        ]
-     regiSel = regions[params.region] ?: error("unknown region ${params.region}")
-    res_sel = resources[params.resource] ?: error("unknown resource ${params.resource}")
-    res_na = params.res_name ?: error("resource name is required")
-    echo "${regiSel}"
-    echo "${res_sel}"
-    echo "${res_na}"
+// helper that returns translated values for region and resource
+// call: def (regiSel, res_sel) = AWSResources(params.region, params.resource)
+
+def call(String region, String resource) {
+    def regions = [
+        'N. Virginia' : 'us-east-1',
+        'Ohio'        : 'us-east-2',
+        'N. Carolina' : 'us-west-1',
+        'Oregon'      : 'us-west-2',
+        'Mumbai'      : 'ap-south-1',
+        'Singapore'   : 'ap-southeast-1',
+        'Sydney'      : 'ap-southeast-1',
+        'Tokyo'       : 'ap-northeast-1',
+        'Frankfurt'   : 'eu-central-1',
+        'London'      : 'eu-west-2',
+        'Stockholm'   : 'eu-north-1',
+        'São Paulo'   : 'sa-east-1'
+    ]
+    def resources = [
+        vpc       : 'vpc',
+        instance  : 'instance',
+        container : 'container',
+        s3        : 's3'
+    ]
+
+    def regiSel = regions[region]
+    if (!regiSel) {
+        error "Unknown region: ${region}"
     }
+
+    def res_sel = resources[resource]
+    if (!res_sel) {
+        error "Unknown resource: ${resource}"
+    }
+
+    return [regiSel, res_sel]
 }
